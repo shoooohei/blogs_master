fb5fb4d1f47d2f1d5e5bbf6a34528a8030c7e24d
Shohei <shoheimoment@gmail.com>
2017-11-21 00:00:48 +0900

    modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to blogs_path
     else
       render 'new'
     end
   end
 
-  # def show
-  #   @user = User.find(params[:id])
-  # end
 
   private
   def user_params
