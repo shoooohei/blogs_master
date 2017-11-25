f654a63d738befc6b05cbc0a16a01ab511460151
Shohei Kawasaki <shoheikawasaki@Shoheis-Mac.local>
2017-11-25 10:00:53 +0900

    complete

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
-  validates :title, :content, presence: true
+  validates :title, :content, presence: true,
+            length:{ maximum: 140,
+                    too_long: "は最大%{count}文字まで使用できます"}
 
   belongs_to :user
   has_many :favorites, dependent: :destroy
